f79d4b13c4f670502869e92c940860ee736cdb71
Oskar Kalaš <o.kalas@asiana.cz>
2025-09-26 22:56:40 +0200

    feat(prisma): add Pothos generator configuration and types integration

--- packages/prisma/pothos.config.js
+++ packages/prisma/pothos.config.js
@@ -1,11 +1,11 @@
 /** @type {import('prisma-generator-pothos-codegen').Config} */
 module.exports = {
   inputs: {
-    outputFilePath: '@xingu/prisma/graphql/__generated__/inputs.ts',
+    outputFilePath: './packages/prisma/src/lib/graphql/inputs.ts',
   },
   crud: {
-    outputDir: './packages/prisma/src/lib',
-    inputsImporter: `import * as Inputs from '@xingu/prisma/graphql/__generated__/inputs';`,
+    outputDir: './packages/prisma/src/lib/graphql',
+    inputsImporter: `import * as Inputs from './packages/prisma/src/lib/graphql/inputs';`,
     resolverImports: `import prisma from '@xingu/prisma';`,
     prismaCaller: 'prisma',
   },

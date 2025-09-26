99172eaef9ab2c30d0f2f6c02a136a53cc2de3a6
Oskar Kalaš <o.kalas@asiana.cz>
2025-09-26 19:08:30 +0200

    feat(api): update paths and configuration for api folder

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/apps'),
+    path: join(__dirname, '../dist/api'),
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
